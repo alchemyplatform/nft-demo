09e39cf90fd95fa7c632b3af8b47f66a6644859a
Hien Vien <hien@alchemy.com>
2023-09-18 17:00:16 -0700

    Add github pages deploy

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
+const githubPages = process.env.DEPLOY_TARGET === 'gh-pages'
+
+const githubPagesConfig = {
+    output: "export",
+    basePath: "/nft-demo",
+    images: {
+        unoptimized: true,
+    }
+}
+
 const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = githubPages ? githubPagesConfig : nextConfig
